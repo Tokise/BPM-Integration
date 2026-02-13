b246cd510acf69eab0b7ba7e11ffea2aecf8323e
Rei <ablenjohnrey@gmail.com>
2026-02-14 04:33:39 +0800

    added and rm sql

--- debug_counts.sql
+++
@@ -1,9 +0,0 @@
--- Check counts only to avoid revealing sensitive data if any
-SELECT 
-  (SELECT count(*) FROM "bpm-anec-global".shops) as shop_count,
-  (SELECT count(*) FROM "bpm-anec-global".products) as product_count,
-  (SELECT count(*) FROM "bpm-anec-global".categories) as category_count;
-
--- Check if RLS is enabled
-SELECT tablename, rowsecurity FROM pg_tables 
-WHERE schemaname = 'bpm-anec-global' AND tablename IN ('shops', 'products');
